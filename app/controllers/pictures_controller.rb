b20bb87a04f14f866f87ef8cfaa691facee681a1
George G <ggogarten@gmail.com>
2016-03-03 13:39:12 -0500

    fixing crud

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,73 @@
+class PicturesController < ApplicationController
+
+	def upload
+		@picture = Picture.new
+
+	end
+
+	def create
+		user = User.find(params[:user_id])
+		house = House.find(params[:house_id])
+
+		if params[:picture][:image]
+			params[:picture][:image].each do |image|
+				@picture = user.pictures.build(image: image)
+				@picture.house_id = house.id
+				@picture.save
+				person = @picture.user.first_name + " " + @picture.user.last_name
+				house_name = @picture.house.name
+				url = @picture.image.url
+				Picture.enroll(url, person, house_name)
+			end
+			p 'picture saved'
+			redirect_to root_path
+		else
+			p 'picture didnt save'
+		end
+	end
+
+	def matchPage
+		@picture = Picture.new
+	end
+
+	def match
+		user = User.find(params[:user_id])
+		house = House.find(params[:house_id])
+		if params[:picture][:image][0]
+			@picture = user.pictures.build(image: params[:picture][:image][0])
+			@picture.house_id = house.id
+			@picture.save
+			url = @picture.image.url
+			house_name = @picture.house.name
+			number = current_user.phone_number
+			Picture.recognize(url, house_name)
+    	redirect_to root_path
+    end
+	end
+
+  def remove_subject
+		user = User.find(params[:user_id])
+		house = House.find(params[:house_id])
+		if params[:picture][:image][0]
+			@picture = user.pictures.build(image: params[:picture][:image][0])
+			@picture.house_id = house.id
+      person = @picture.user.first_name + " " + @picture.user.last_name
+			house_name = @picture.house.name
+			Picture.remove_subject(person, house_name)
+    	redirect_to root_path
+    end
+	end
+
+
+	def lock
+		lock = 'lock'
+		Picture.text(lock)
+		redirect_to root_path
+	end
+
+  private
+
+  def pic_params
+  	params.require(:picture).permit(:image)
+  end
+end
---
+++ app/models/picture.rb
@@ -0,0 +1,89 @@
+class Picture < ActiveRecord::Base
+	has_attached_file :image, styles: { large: "400x400" }
+
+  belongs_to :user
+  belongs_to :house
+
+  validates_attachment :image, content_type: { content_type: ["image/jpeg", "image/png"]}
+
+  def self.enroll(url, person, house_name)
+
+
+    body = {:image => url, :subject_id => person, :gallery_name => house_name, :selector => "SETPOSE", :symmetricFill => "true"  }
+    body = body.to_json
+
+    headers = {
+      'content_type' => 'application/json',
+      'app_id' => ENV['KAIROS_ID'],
+      'app_key' => ENV['KAIROS_KEY']
+    }
+
+    response = HTTParty.post('https://api.kairos.com/enroll', { headers: headers, body: body })
+    ap response
+  end
+
+  def self.recognize(url, house_name)
+
+    threshold = '0.80'
+
+    body = {:image => url, :gallery_name => house_name, :threshold => threshold}
+    body = body.to_json
+
+
+    headers = {
+      'content_type' => 'application/json',
+      'app_id' => ENV['KAIROS_ID'],
+      'app_key' => ENV['KAIROS_KEY']
+    }
+
+    response = HTTParty.post('https://api.kairos.com/recognize', { body: body, headers: headers})
+
+    ap response
+
+    successtest = response['images'][0]['transaction']['status']
+    # ap successtest
+    successtest
+    Picture.text(successtest)
+  end
+
+  def self.remove_subject(person, house_name)
+
+    body = {:gallery_name => house_name, :subject_id => person}
+    body = body.to_json
+
+    headers = {
+      :content_type => 'application/json',
+      :app_id => ENV['KAIROS_ID'],
+      :app_key => ENV['KAIROS_KEY']
+    }
+
+    response = HTTParty.post('https://api.kairos.com/gallery/remove_subject', { body: body, headers: headers})
+    ap response
+  end
+
+
+
+  def self.text(status)
+    client = Twilio::REST::Client.new ENV['account_sid'], ENV['auth_token']
+    from = '+17863726460'
+    if status == 'success'
+      client.account.messages.create(
+        :from => from,
+        :to => "+13053262790",
+        :body => 'unlock'
+        )
+		elsif status == 'lock'
+			client.account.messages.create(
+        :from => from,
+        :to => "+13053262790",
+        :body => 'lock'
+        )
+    else
+      client.account.messages.create(
+        :from => from,
+        :to => "+13053262790",
+        :body => "Face Didn't Match"
+        )
+    end
+  end
+end
